f417cb7d0b989a46ab8275fa2b8498e499c5d291
Evan Burt <evantburt@gmail.com>
2017-11-11 09:37:57 -0600

    set requirements versions

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'pySerial',
-        'click',
+        'pySerial==3.4',
+        'click==6.7',
     ],
     entry_points='''
         [console_scripts]
